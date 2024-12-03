32c9e13973115bdf5167c94771f762aec7592d40
Priyo Raihan <priyoraihan@gmail.com>
2024-12-03 16:34:40 +0600

    Fix Bug: Make TransactionEvent serializable

--- classes/TransactionEvent.java
+++ classes/TransactionEvent.java
@@ -1,6 +1,8 @@
 package classes;
 
-public class TransactionEvent {
+import java.io.Serializable;
+
+public class TransactionEvent implements Serializable {
     private String type;
     private double amount;
     private String platform;
